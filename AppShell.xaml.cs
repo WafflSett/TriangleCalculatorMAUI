b41ace70800a08c8028b321a69e1139293ec8554
WaffleTG <toros.gergo@students.jedlik.eu>
2025-09-27 10:15:47 +0200

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TriangleCalculatorMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
